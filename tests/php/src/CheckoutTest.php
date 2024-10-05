1eac40e47d4f54338a0ac942ef1f5c3275156e3d
Greys <4136890+jgreys@users.noreply.github.com>
2024-10-06 06:40:23 +0900

    Fix - Unit tests

---
+++ tests/php/src/CheckoutTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * These teste make assertions against class WC_Korea_Test.
+ *
+ * @package Greys\WooCommerce\Korea\Tests\Checkout
+ */
+
+namespace Greys\WooCommerce\Korea\Tests;
+
+use Greys\WooCommerce\Korea\Checkout\Helper;
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+
+/**
+ * CheckoutTest class.
+ */
+class CheckoutTest extends TestCase {
+
+	public function set_up(): void {
+        parent::set_up();
+    }
+
+	public function test_format_phone() {
+		$this->assertEquals( '1588-0000', Helper::format_phone( '1588-0000' ) );
+		$this->assertEquals( '02-345-6789', Helper::format_phone( '+82 23456789' ) );
+		$this->assertEquals( '02-3456-7891', Helper::format_phone( '+82 23 456 7891' ) );
+		$this->assertEquals( '02-2345-6789', Helper::format_phone( '822 2345 6789' ) );
+		$this->assertEquals( '02-3000-5000', Helper::format_phone( '0230005000' ) );
+		$this->assertEquals( '031-222-3333', Helper::format_phone( '82-0312-2233-33' ) );
+		$this->assertEquals( '031-2222-3333', Helper::format_phone( '03122223333' ) );
+		$this->assertEquals( '011-444-5555', Helper::format_phone( '011 444 5555' ) );
+		$this->assertEquals( '010-6666-7777', Helper::format_phone( '82+1066667777' ) );
+		$this->assertEquals( '0303-456-7890', Helper::format_phone( '03034567890' ) );
+		$this->assertEquals( '0505-987-6543', Helper::format_phone( '050-5987-6543' ) );
+		$this->assertEquals( '0303-4567-8900', Helper::format_phone( '030345678900' ) );
+		$this->assertEquals( '0505-9876-5432', Helper::format_phone( '050-5987-65432' ) );
+		$this->assertEquals( '070-7432-1000', Helper::format_phone( '0707-432-1000' ) );
+	}
+
+	public function test_is_valid_phone() {
+		$this->assertTrue( Helper::is_valid_phone( '1588-0000' ) );
+		$this->assertTrue( Helper::is_valid_phone( '02-345-6789' ) );
+		$this->assertTrue( Helper::is_valid_phone( '+82-2-345-6789' ) );
+		$this->assertTrue( Helper::is_valid_phone( '+82-02-2345-6789' ) );
+		$this->assertTrue( Helper::is_valid_phone( '053-444-5555' ) );
+		$this->assertTrue( Helper::is_valid_phone( '053-4444-5555' ) );
+		$this->assertTrue( Helper::is_valid_phone( '011-444-5555' ) );
+		$this->assertTrue( Helper::is_valid_phone( '010-4444-5555' ) );
+		$this->assertTrue( Helper::is_valid_phone( '0303-4444-5555' ) );
+		$this->assertTrue( Helper::is_valid_phone( '0505-4444-5555' ) );
+
+		$this->assertFalse( Helper::is_valid_phone( '010-4444-55555' ) );
+		$this->assertFalse( Helper::is_valid_phone( '010-1234-5678' ) );
+		$this->assertFalse( Helper::is_valid_phone( '02-123-4567' ) );
+		$this->assertFalse( Helper::is_valid_phone( '02-123456' ) );
+		$this->assertFalse( Helper::is_valid_phone( '03-456-7890' ) );
+		$this->assertFalse( Helper::is_valid_phone( '090-9876-5432' ) );
+		$this->assertFalse( Helper::is_valid_phone( '0303-1111-5432' ) );
+		$this->assertFalse( Helper::is_valid_phone( '0505-9876-543210' ) );
+	}
+
+}
